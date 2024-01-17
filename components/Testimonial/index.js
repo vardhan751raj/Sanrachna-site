5e466afca2ff4018a797290ad0499e0edee9ee33
vardhan751raj <150066027+vardhan751raj@users.noreply.github.com>
2024-01-18 00:17:55 +0530

    Update index.js

--- components/Testimonial/index.js
+++ components/Testimonial/index.js
@@ -43,35 +43,40 @@ const customerReviews = [
     id: 1,
     // photo: "url_to_customer1_photo.jpg",
     photo: './images/last.jpg',
-    profession: 'Abhishek Kuma',
+    profession: 'Army Officer',
+    name: 'Mr. Abhishek Kumar",
     text: 'Sanrachna Co. did a top notch renovation and new construction on our home. We love it and are very pleased with the results.'
   },
   {
     id: 2,
     // photo: 'url_to_customer2_photo.jpg',
     photo: './images/last.jpg',
-    profession: 'Ajeet Pandey',
+    profession: 'Businessman';
+    name: 'Mr. Ajeet Pandey',
     text: 'They made what could’ve been quite disruptive a very smooth experience, and the final renovations look great. Thanks for the great design help and quality form the whole team.'
   },
   {
     id: 3,
     // photo: 'url_to_customer2_photo.jpg',
     photo: './images/last.jpg',
-    profession: 'Virendra Choudhary',
+    profession: 'Lawyer' ,
+    name: 'Mr. Virendra Choudhary',
     text: 'We strongly felt that we should go to Sanrachna Construction rather than any other contractor, and I would highly recommend Sanrachna Co. to my own family and friends.'
   },
   {
     id: 4,
     // photo: 'url_to_customer2_photo.jpg',
     photo: './images/last.jpg',
-    profession: 'Dr. Vikas Patel',
+    profession: 'Doctor',
+    name: 'Dr. Vikas Patel',
     text: 'Sanrachna Co. made no false promises and we were actually happy to have found Sanrachna Co. It was an online search however, Sanrachna Co. was very transparent and that's what we liked the most and now we are happy homeowners. Thank you Sanrachna Co.'
   },
   {
     id: 5,
     // photo: 'url_to_customer2_photo.jpg',
     photo: './images/last.jpg',
-    profession: 'Dr. Sudhir Nagar',
+    profession:'Doctor', 
+    name : 'Dr. Sudhir Nagar',
     text: 'The experience was great, and I am happy and satisfied. Sanrachna did a fantastic job, they were quick, efficient, and very professional'
   }
 
